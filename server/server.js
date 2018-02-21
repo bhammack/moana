2170ea8250775641757dced3b5d49d9a1de2d306
Ben Hammack <benjamin.hammack@lmco.com>
2018-02-21 15:34:41 -0500

    Creating client npm repo

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ mongoose.connect(database.url, {}).then(
     console.log('mongoose connected');
   }, // connection ready to use.
   err => {
-    console.log(err);
+	console.log('unable to connect to mongo.db instance');
+    //console.log(err);
   }
 );
 
@@ -25,7 +26,8 @@ const app = express();
 
 app.use(bodyParser.json());                                   // parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));          // parse application/x-www-form-urlencoded
-app.use(express.static(path.join(__dirname, '../client/dist')));        // set the static files location (/public/js will be /js for clients)
+//app.use(express.static(path.join(__dirname, '../client/dist')));        // set the static files location (/public/js will be /js for clients)
+app.use(express.static(path.join(__dirname, '../client/static')));        // set the static files location (/public/js will be /js for clients)
 
 // API configuration ======================================================================================================================
 const api = require('./routes/api');                   // Get our API router object.
@@ -33,7 +35,7 @@ app.use('/api', api);                                         // Set our api rou
 
 // Express Routes =========================================================================================================================
 app.get('*', (req, res) => {                                  // Catch all other routes and return the index file
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 // Server start ===========================================================================================================================
