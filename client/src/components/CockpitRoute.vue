467c957eb100c87fae8bae8e5e9cc7f6240273fe
Ben Hammack <benjamin.hammack@lmco.com>
2018-02-26 16:52:52 -0500

    Added radial and linear gauge placeholders

--- client/src/components/CockpitRoute.vue
+++ client/src/components/CockpitRoute.vue
@@ -2,25 +2,31 @@
     <div id="cockpit">
         
         <div class="video">
-            <iframe 
+            <!-- <iframe 
                 src="https://www.youtube.com/embed/tgbNymZ7vqY" 
                 frameborder="1" 
                 style="display: block; width: 100%; height: 100%;">
-            </iframe>
+            </iframe> -->
         </div>
         <div class="map">
-            <iframe 
+            <!-- <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d14019.203099096716!2d-81.21332595000001!3d28.54570735!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1519679998787" 
                 frameborder="1" 
                 style="display: block; width: 100%; height: 100%;">
-            </iframe>
+            </iframe> -->
         </div>
-        <div class="sensor">
+        <div class="sensor sensor1">
+            <radial-gauge :value="377"></radial-gauge>
+        </div>
+        <div class="sensor sensor2">
+            <radial-gauge :value="377"></radial-gauge>
+        </div>
+        <div class="sensor sensor3">
             <linear-gauge :value="377"></linear-gauge>
         </div>
-        <div class="sensor"></div>
-        <div class="sensor"></div>
-        <div class="status"></div>
+        <div class="status">
+            <h1>Status bar here</h1>
+        </div>
 
 
         
@@ -57,8 +63,17 @@
         grid-area: map;
     }
 
-    .sensor {
+
+    .sensor.sensor1 {
         grid-area: sensor1;
+    }
+
+    .sensor.sensor2 {
+        grid-area: sensor2;
+    }
+
+    .sensor.sensor3 {
+        grid-area: sensor3;
     }
 
     .status {
