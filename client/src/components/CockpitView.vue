ca97e23c6736bae5e4dc187815f6b9ccdb0d161b
Ben Hammack <benjamin.hammack@lmco.com>
2018-02-27 14:22:30 -0500

    Added favicon. Need to figure out how to make it link properly

--- client/src/components/CockpitView.vue
+++ client/src/components/CockpitView.vue
@@ -2,18 +2,18 @@
     <div id="cockpit">
         
         <div class="video">
-            <!-- <iframe 
+            <iframe 
                 src="https://www.youtube.com/embed/tgbNymZ7vqY" 
                 frameborder="1" 
                 style="display: block; width: 100%; height: 100%;">
-            </iframe> -->
+            </iframe>
         </div>
         <div class="map">
-            <!-- <iframe 
+            <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d14019.203099096716!2d-81.21332595000001!3d28.54570735!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1519679998787" 
                 frameborder="1" 
                 style="display: block; width: 100%; height: 100%;">
-            </iframe> -->
+            </iframe>
         </div>
         <div class="sensor sensor1">
             <radial-gauge :value="377"></radial-gauge>
