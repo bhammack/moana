52b0d6f646d1ab8e001adb3cc64bbd1b9376f72d
Ben Hammack <benjamin.hammack@lmco.com>
2018-03-16 18:07:17 -0400

    Updated model to inclulde pos info

--- server/models/telemetry.js
+++ server/models/telemetry.js
@@ -4,11 +4,23 @@ var Schema = mongoose.Schema;
 // Mongo auto generates the _id field for each element in the collection.
 var TelemetrySchema = new mongoose.Schema({
     dateReceived: { type: Date, default: Date.now },
-    temperature: Number,
-    altitude: Number,
-    power: Number
-    //latitude: Number,
-    //longitude: Number,
+    temperature: Number,        // degrees f
+    altitude: Number,           // feet
+    power: Number,              // [0-100] percentage as integer
+    latitude: Number,           // relative or absolute
+    longitude: Number,          // relative or absolute
+    eventCode: Number           // enumerable
 });
 
 module.exports = mongoose.model('Telemetry', TelemetrySchema);
+
+
+/*
+Need to store:
+Altitude - in feet
+Air Temperature - celsius, however the user needs the ability to change the units.
+Power system health - remaining battery percentage and the remaining flight time
+Latitude (relative or absolute)
+Longitude (relative or absolute)
+Event codes - up to us for any other data we need to declare.
+*/
