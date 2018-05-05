875a399e8319879b571f2ecebc5e9213365a9159
Ben Hammack <hammack.b@gmail.com>
2018-05-05 19:59:54 +0000

    mavlink success! Adding test code

--- scripts/mavlink.py
+++ scripts/mavlink.py
@@ -33,17 +33,44 @@ MAV> APM: PreArm: RC Roll not configured
 
 from pymavlink import mavutil
 
-BAUD_RATE = 57600 # 115200
-#DEVICE = 'udp:127.0.0.1:14550'
-#PORT = 'COM3'
-DEVICE = '/dev/ttyACM0'
-#DEVICE = 'COM3-1-QUADROTOR'
-# 115200
+BAUD_RATE = 11520
+DEVICE = '/dev/ttyACM0' # this is actually a usb interface
+
 
 
 print('Awaiting heartbeat...')
-
 conn = mavutil.mavlink_connection(device=DEVICE, baud=BAUD_RATE, autoreconnect=True)
 conn.wait_heartbeat()
-
 print('We have a pulse!')
+print('Requesting access to the mavlink data stream...')
+conn.mav.request_data_stream_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, BAUD_RATE, True)
+
+while(True):
+    try:
+		# enter what appears to be a non-blocking loop. A blocking mode seems available.
+		msg = conn.recv_match(blocking=False)
+		if not msg: 
+			continue
+
+		msg_type = msg.get_type()
+
+		if (msg_type == "HEARTBEAT"):
+			print("heartbeat")
+			mode = mavutil.mode_string_v10(msg)
+			is_armed = msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED
+			is_enabled = msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_GUIDED_ENABLED
+
+		if (msg_type == "ATTITUDE"):
+			attitude_data = (msg.roll, msg.pitch, msg.yaw, msg.rollspeed, msg.pitchspeed, msg.yawspeed)
+			print "Roll\tPit\tYaw\tRSpd\tPSpd\tYSpd"
+			print "%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t" % attitude_data
+
+		if (msg_type == "VFR_HUD"):
+			hud_data = (msg.airspeed, msg.groundspeed, msg.heading, msg.throttle, msg.alt, msg.climb)
+			print "Aspd\tGspd\tHead\tThro\tAlt\tClimb"
+			print "%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f" % hud_data
+			
+    except KeyboardInterrupt:
+		conn.close()
+		break
+
