950b5f05ca347ced63d668056e4e920fa27a8059
Ben Hammack <benjamin.hammack@lmco.com>
2018-05-16 11:08:27 -0400

    changes to about page

--- client/src/components/AboutView.vue
+++ client/src/components/AboutView.vue
@@ -1,13 +1,23 @@
 <template>
     <div id="about">
         <h1>About</h1>
-        <h6 v-for="name in names">{{name}}</h6>
-
-
-
-
-
-
+        <div class="container">
+            <div class="jumbotron">
+                <div class="row">
+                    <div class="col-6">
+                    </div>
+                    <div class="col-6">
+                        <ul>
+                            <li v-bind:key="name" v-for="name in names">
+                                <h6>{{name}}</h6>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            
+            </div>
+            
+        </div>
     </div>
 </template>
 <script>
@@ -15,12 +25,12 @@
         data: function() {
             return {
                 names: [
-                    'Ben Hammack',
-                    'Phillip Bent',
-                    'Mark Shipps',
                     'Ray Sanzi III',
                     'Seaver Wrisley',
                     'Matt Jean',
+                    'Ben Hammack',
+                    'Phillip Bent',
+                    'Mark Shipps',
                     'Ayo Duyile',
                     'Chris Parmentier',
                     'Jill Sharkley',
