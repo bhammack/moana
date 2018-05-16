4b481b1b97455c2f98f23a4ff7228f27302739f6
Ben Hammack <hammack.b@gmail.com>
2018-05-16 03:03:32 +0000

    sdd

--- scripts/xbeerx.py
+++ scripts/xbeerx.py
@@ -1,6 +1,6 @@
 # GPIO14 (pin 8) is TX
 # GPIO15 (pin 10) is RX
-from xbee import XBee
+#from xbee import XBee
 import serial
 
 # PORT='/dev/ttyAMA0'
@@ -9,19 +9,12 @@ PORT='/dev/ttyS0'
 BAUD=9600
 
 ser = serial.Serial(PORT, BAUD)
-'''
-xbee = XBee(ser)
+
 while True:
     try:
-        response = xbee.wait_read_frame()
-        print response
+        data = ser.read(128)
+        print(data)
     except KeyboardInterrupt:
         break
-'''
-
-
-print(ser.read())
-
-
 
 ser.close()
